3688f2add006ec7fc5b0ab051810b6e1a924a97e
Jiwei Bi <bibijijiweihch@gmail.com>
2017-10-13 14:27:38 -0700

    add php relation

--- query.php
+++ query.php
@@ -4,13 +4,15 @@
 </head>
 
 <body>
-	
+
 	<form action ="" method="GET">
 		<TEXTAREA NAME="query" ROWS=10 COLS=50> 
 			<?php if (isset($_GET["query"])) echohtmlspecialchars($_GET["query"]);?>
 		</TEXTAREA><br />
 		<input type="submit" value="Submit" />
-	</form>>
+	</form>
+
+</body>>
 
 	<?php 
 		if (!isset($_GET["query"]) || $_GET["query"] === "")
@@ -25,13 +27,24 @@
 		}
 
 		//select a specific database
-		mysql_select_db("TEST", $db_connection);
+		$db_selected = mysql_select_db("CS143", $db_connection);
+		if(!$db_selected) {
+   			$errmsg = mysql_error($db_selected);
+    		print "Connection failed: $errmsg" <br />;
+    		exit(1);
+		}
 
 		//check user input 
-		$sanitized_name = mysql_real_escape_string($name, $db_connection);
-		$query_to_issue = sprintf($query, $sanitized_name);
+		//$sanitized_name = mysql_real_escape_string($name, $db_connection);
+		//$query_to_issue = sprintf($query, $sanitized_name);
 		//issue queries
-		$rs = mysql_query($query_to_issue, $db_connection);
+		$user_sql =  $_GET["query"];
+		$rs = mysql_query($user_sql, $db_connection);
+		if(!$rs) {
+			$errmsg = mysql_error($rs);
+    		print "Connection failed: $errmsg" <br />;
+    		exit(1);
+		}
 
 		//print result
 		echo "fetch result"
@@ -40,9 +53,6 @@
 		mysql_close($db_connection);
 
 		//retrieving results
-
-	?>
-
-</body>>
+    ?>
 
 </html>
