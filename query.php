507ee3c8ecb5e462f3fd42e45299a0669ebcb555
Jiwei Bi <bibijijiweihch@gmail.com>
2017-10-13 14:55:11 -0700

    fixed

--- query.php
+++ query.php
@@ -10,25 +10,52 @@
 
     <p>
 		<form action ="" method="GET">
-			<textarea> NAME="query" rows=10 cols=50> 
+			<textarea NAME="query" rows=10 cols=50> 
 				<?php if (isset($_GET["query"])) echohtmlspecialchars($_GET["query"]);?>
 			</textarea><br />
 			<input type="submit" value="Submit" />
 		</form>
 
 		 <?php
-        if (!isset($_GET["query"]) || $_GET["query"] === "")
-            die("No query entered.");
-        $db = mysql_connect("localhost", "cs143", "");
-        if (!$db)
-            die("Unable to connect to database: " . mysql_error());
-        $db_selected = mysql_select_db("CS143", $db);
-        if (!$db_selected)
-            die("Unable to select database: " . mysql_error());
-        $sql = $_GET["query"];
-        if (!$result = mysql_query($sql))
-            die("Error executing query: " . mysql_error());
-       
+        	if (!isset($_GET["query"]) || $_GET["query"] === "")
+        		die("No query entered.");
+		
+			//connect to local machine with username cs143 and empty pw
+			$db_connection = mysql_connect("localhost", "cs143", "");
+			if(!$db_connection) {
+   				$errmsg = mysql_error($db_connection);
+    			print "Connection failed: $errmsg" <br />;
+    			exit(1);
+			}
+
+			//select a specific database
+			$db_selected = mysql_select_db("CS143", $db_connection);
+			if(!$db_selected) {
+   				$errmsg = mysql_error($db_selected);
+    			print "Connection failed: $errmsg" <br />;
+    			exit(1);
+			}
+
+			//check user input 
+			//$sanitized_name = mysql_real_escape_string($name, $db_connection);
+			//$query_to_issue = sprintf($query, $sanitized_name);
+			//issue queries
+			$user_sql =  $_GET["query"];
+			$rs = mysql_query($user_sql);
+			if(!$rs) {
+				$errmsg = mysql_error($rs);
+    			print "Connection failed: $errmsg" <br />;
+    			exit(1);
+			}
+
+			//print result
+			echo "<h3>Results from MySQL:</h3>\n";
+
+			//release result
+			mysql_free_result($result);
+			//close datavase
+			mysql_close($db_connection);
+
    		?>
 	</p>
 </body>>
