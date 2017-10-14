38dbdf20cbaf7adf445d3ce95cd2fa1f2d016ff5
Jiwei Bi <bibijijiweihch@gmail.com>
2017-10-14 13:43:56 -0700

    add constraints

--- create.sql
+++ create.sql
@@ -1,69 +1,75 @@
 CREATE TABLE Movie(
-	id INT primary key,   -- Each movie has its own id
-	title VARCHAR(100),
+	id INT,   
+	title VARCHAR(100) NOT NULL, --Every movie must have a title.
 	year INT,
 	rating VARCHAR(10),
-	company VARCHAR(50)
+	company VARCHAR(50),
+	PRIMARY KEY(id) -- Primary 1: Every movie has a unique id.
 ); 
 
 CREATE TABLE Actor(
-	-- ID is unique to all people (which is shared between actors and directors)
-	-- if a person is both an actor and a director, the person will have the same 
-	-- ID both in the Actor and the Director table
-	id INT primary key, 
+	id INT, 
 	last VARCHAR(20),
 	first VARCHAR(20),
 	sex VARCHAR(6),
-	dob DATE,
-	dod DATE
+	dob DATE NOT NULL, -- Every actor must have a date of birth.
+	dod DATE,
+	PRIMARY KEY(id) -- Primary key 2: Every actor has a unique id
 );
 
 CREATE TABLE Sales(
-	mid INT primary key, -- Each movie has its own id
+	mid INT,  
 	ticketsSold INT,
-	totalIncome INT
-);
+	totalIncome INT,
+	FOREIGN KEY (mid) references Movie(id)  -- Reference 1
+)ENGINE=INNODB;
 
 CREATE TABLE Director(
-	-- ID is unique to all people (which is shared between actors and directors)
-	-- if a person is both an actor and a director, the person will have the same 
-	-- ID both in the Actor and the Director table
-	id INT primary key, 
+	id INT,
 	last VARCHAR(20),
 	first VARCHAR(20),
 	sex VARCHAR(6),
 	dob DATE,
-	dod DATE 
+	dod DATE,
+	PRIMARY KEY(id)-- Primary key 3: Every director has a unique id 
 );
 
 CREATE TABLE MovieGenre(
-	mid INT primary key, -- Each movie has its own id
-	genre VARCHAR(20)
-);
+	mid INT,  
+	genre VARCHAR(20),
+	FOREIGN KEY (mid) references Movie(id)  -- Reference 2
+)ENGINE=INNODB;
 
 CREATE TABLE MovieDirector(
-	mid INT primary key, -- Each movie has its own id
-	did INT
-);
+	mid INT, 
+	did INT, 
+	FOREIGN KEY (mid) references Movie(id)  -- Reference 3
+	FOREIGN KEY (did) references Director(id)  -- Reference 4
+)ENGINE=INNODB;
 
 CREATE TABLE MovieActor(
-	mid INT primary key, -- Each movie has its own id
+	mid INT,  
 	aid INT,
-	role VARCHAR(50)
-);
+	role VARCHAR(50)，
+	FOREIGN KEY (mid) references Movie(id)  -- Reference 5
+	FOREIGN KEY (aid) references Actor(id)  -- Reference 6
+)ENGINE=INNODB;
 
 CREATE TABLE MovieRating(
-	mid INT primary key, -- Each movie has its own id
+	mid INT, 
 	imdb INT,
-	rot INT
+	rot INT,
+	CHECK(imdb >= 0 AND rating <= 100)  --Check 1: imdb is 0 - 100
+	CHECK(rot >= 0 AND rating <= 100)  --Check 2: rot is 0 - 100
 );
 
 CREATE TABLE Review(
 	name VARCHAR(20),
 	time TIMESTAMP,
-	mid INT primary key, -- Each movie has its own id
+	mid INT, 
 	rating INT,
-	comment VARCHAR(500)
+	comment VARCHAR(500)，
+    CHECK(rating >= 0 AND rating <= 5)  --Check 3: rating is 0 - 5
 );
 
 CREATE TABLE MaxPersonID(
