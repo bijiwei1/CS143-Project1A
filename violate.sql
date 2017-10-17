42dd568c07793e0f6441b4e9f1696d767be4b702
Haoran Wang <wanghran@gmail.com>
2017-10-16 17:04:23 -0700

    Add files via upload

--- violate.sql
+++ violate.sql
@@ -1,26 +1,40 @@
 INSERT INTO Movie
-	VALUES(\N, "killa kill", 2018, "R", "Warner Sisters"); --It violates the primary key constraint that it cannot be null
+	VALUES(1002, "killa kill", 2018, "R", "Warner Sisters"); --It violates the primary key constraint for id
+--error 1062 (23000): Duplicate entry '1002' for key 'Primary'
 
 INSERT INTO Actor
-	VALUES(\N, "Wang", "Haoran", "Male", 1995-03-23, \N); --It violated the primary key constraint that it cannot be null
-
-INSERT INTO Sales
-	VALUES(\N, 400000, 9000000000); --It violates the constraint that movie id cannot be null
+	VALUES(1002, "Wang", "Haoran", "Male", 1995-03-23, \N); --It violated the primary key constraint for id
+--error 1062 (23000): Duplicate entry '1002' for key 'Primary'
 
 INSERT INTO Director
-	VALUES(\N, "Wang", "Haoran", "Male", 1995-03-23, \N); --It violates the constraint that director id cannot be null
+	VALUES(300, "Wang", "Haoran", "Male", 1995-03-23, \N); --It violates the primary key constraint for id
+--error 1062 (23000): Duplicate entry '1002' for key 'Primary'
 
-INSERT INTO MovieGenre
-	VALUES(\N, "drama"); --It violates the constraint that the movie id cannot be null
 
-INSERT INTO MovieDirector
-	VALUES(\N, 3496); --It violates the constraint that the movie id cannot be null
+DELETE FROM Director WHERE id = 1019; --It violates the foreign key constraint for Movie Director
+--error 1451 (23000): cannot delete or update a parent row: a foreign key constraint fail
+
+DELETE FROM Movie where id = 598; --It violates the foreign key constraint for Sales
+--error 1451 (23000): cannot delete or update a parent row: a foreign key constraint fail
 
 INSERT INTO MovieActor
-	VALUES(\N, 234122, "Hero"); --It violates the constraint that the movie id cannot be null
+	VALUES(2001, 100, 'rua'); --It violates the foreign key constraint for aid
+--error 1452 (23000): Cannot add or update a child row: a foreign key constraint fails
 
-INSERT INTO MovieRating
-	VALUES(2341234, 200, 99); --It violates the check that imdb has to be between 0 and 100
+DELETE FROM Actor WHERE id = 182;--it violates the foreign key constraint for MovieActor aid
+--error 1451 (23000): cannot delete or update a parent row: a foreign key constraint fail
+
+INSERT INTO MovieDirector
+	VALUES(1002, 1000); --It violates the foreign key constraint for did
+--error 1452 (23000): Cannot add or update a child row: a foreign key constraint fails
+
+DELETE FROM Movie where id = 1002; --It violates the foreign key constraint for MoiveDirector mid
+--error 1451 (23000): cannot delete or update a parent row: a foreign key constraint fail
+
 
 INSERT INTO Review
-	VALUES("WOW", 2017-10-15 10:10:10, 283423, 10, "wow"); --It voilates the check that the rating has to be between 0 and 5
+	VALUES('ra', 2009-10-10 20:20:20, 1002, 10 'ww');--it violates the check constraint that rating has to be between 0 and 5
+
+INSERT INTO MovieRating
+	VALUES(1002, 200, 200);--It violates the check constraints: first imdb has to be between 0 and 200;
+	--second, rot has to be between 0 and 100. 
